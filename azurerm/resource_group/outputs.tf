ed909f1a76e7e3d8f53f283fc4e7c0fd82bbf054
gmnochez <gmnochez@outlook.com>
2024-05-03 16:08:43 -0400

    test

--- azurerm/resource_group/outputs.tf
+++ azurerm/resource_group/outputs.tf
@@ -1,7 +1,7 @@
 output "default_resource_group_name" {
-  value = azurerm_resource_group.default.name
+  value = azurerm_resource_group.hadley_resource.name
 }
 
 output "default_resource_group_id" {
-  value = azurerm_resource_group.default.id
+  value = azurerm_resource_group.hadley_resource.id
 }
