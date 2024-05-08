452e9ee87a1e8d543169f14236e0c873a1db8db1
gmnochez <gmnochez@outlook.com>
2024-05-08 14:15:52 -0400

    test

--- azurerm/network/subnet/variables.tf
+++ azurerm/network/subnet/variables.tf
@@ -1,19 +1,4 @@
-variable "location" {
-  description = "Location"
-}
 
-variable "resource_group_name" {
-  description = "Resource group name"
-}
-
-variable "main_virtual_network_name" {
-  description = "Virtual network name"
-}
-
-variable "main_virtual_network_address_space" {
-  description = "Virtual network address space"
-  type = set(string)
-}
 
 variable "app_subnet_name" {
   description = "App subnet name"
