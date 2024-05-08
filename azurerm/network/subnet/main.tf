49fb2c399fba54e07e51a5e179e9fb42a56b6892
gmnochez <gmnochez@outlook.com>
2024-05-08 15:05:46 -0400

    test

--- azurerm/network/subnet/main.tf
+++ azurerm/network/subnet/main.tf
@@ -1,5 +1,7 @@
 resource "azurerm_subnet" "hadley_resource" {
   name                 = var.app_subnet_name
+  resource_group_name  = var.app_subnet_resource_group_name
+  virtual_network_name = var.app_subnet_virtual_network_name
   address_prefixes     = var.app_subnet_address_prefixes
- 
+
 }
