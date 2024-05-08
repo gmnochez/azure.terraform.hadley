85016be1e84e383020fe74d6934b8d2d185bf71c
gmnochez <gmnochez@outlook.com>
2024-05-08 14:54:48 -0400

    test

--- azurerm/network/subnet/main.tf
+++ azurerm/network/subnet/main.tf
@@ -1,7 +1,6 @@
 resource "azurerm_subnet" "hadley_resource" {
   name                 = var.app_subnet_name
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.hadley_resource.name
   address_prefixes     = var.app_subnet_address_prefixes
 
   service_endpoints = ["Microsoft.Sql", "Microsoft.Storage", "Microsoft.KeyVault"]
