cbbf75b541aa7d833188e2296d06aa46a2f31b23
gmnochez <gmnochez@outlook.com>
2024-05-08 11:53:34 -0400

    test

--- azurerm/network/virtual_network/variables.tf
+++ azurerm/network/virtual_network/variables.tf
@@ -15,15 +15,4 @@ variable "main_virtual_network_address_space" {
   type = set(string)
 }
 
-variable "app_subnet_name" {
-  description = "App subnet name"
-}
 
-variable "app_subnet_address_prefixes" {
-  description = "App subnet address prefixes"
-  type = set(string)
-}
-
-variable "app_subnet_security_group_name" {
-  description = "App subnet security group name"
-}
