959a180c8d70098f4e6860a0c363b25c1d7ad819
gmnochez <gmnochez@outlook.com>
2024-05-08 17:27:56 -0400

    test

--- azurerm/resource_group/main.tf
+++ azurerm/resource_group/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "hadley_resource" {
   name     = var.resource_group_name
   location = var.location
 
-  tags {
+  tags = {
     for tag in var.tags:
     tag.name_tag = tag.value_tag
     }
