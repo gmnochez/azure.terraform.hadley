15ed1ff5f6f8390f7a7050266fc43fc7b66ff113
gmnochez <gmnochez@outlook.com>
2024-05-08 14:18:36 -0400

    test

--- azurerm/network/subnet/outputs.tf
+++ azurerm/network/subnet/outputs.tf
@@ -1,6 +1,3 @@
-output "hadley_resource_virtual_network_id" {
-  value = azurerm_virtual_network.hadley_resource.id
-}
 
 output "hadley_resource_subnet_id" {
   value = azurerm_subnet.hadley_resource.id
