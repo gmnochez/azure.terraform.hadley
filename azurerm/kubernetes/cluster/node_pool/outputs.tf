a804736eb3ecf19c70156aa07797ae17e067896a
gmnochez <gmnochez@outlook.com>
2024-05-10 09:07:47 -0400

    test

--- azurerm/kubernetes/cluster/node_pool/outputs.tf
+++ azurerm/kubernetes/cluster/node_pool/outputs.tf
@@ -1,3 +1,3 @@
-output "hadley_resource_azurerm_kubernetes_cluster_node_pool_id" {
-  value = azurerm_kubernetes_cluster_node_pool.hadley_resource.id
+output "hadley_resource_azurerm_kubernetes_cluster_node_pool_name" {
+  value = azurerm_kubernetes_cluster_node_pool.hadley_resource.name
 }
