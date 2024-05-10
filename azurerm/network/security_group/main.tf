2e8c5e8bc0414f4a05b4b304583f3e414883aaf7
gmnochez <gmnochez@outlook.com>
2024-05-10 12:07:15 -0400

    test

--- azurerm/network/security_group/main.tf
+++ azurerm/network/security_group/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_network_security_group" "hadley_resource" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
-
+ 
   dynamic "security_rule" {
     for_each = [for rule in var.security_rule : {
       name                       = rule.name
