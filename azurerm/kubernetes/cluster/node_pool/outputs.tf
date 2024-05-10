a7c5b76659bde9eb2a004fcf0c9a8080b8531bc8
gmnochez <gmnochez@outlook.com>
2024-05-10 09:25:58 -0400

    test

--- azurerm/kubernetes/cluster/node_pool/outputs.tf
+++ azurerm/kubernetes/cluster/node_pool/outputs.tf
@@ -1,4 +1,3 @@
 output "hadley_resource_azurerm_kubernetes_cluster_node_pool_id" {
-  # value = azurerm_kubernetes_cluster_node_pool.hadley_resource.id
   value = {for k, v in azurerm_kubernetes_cluster_node_pool.hadley_resource: k => v.id}
 }
