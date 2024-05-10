06e39ab3854184ec5208ca5c6a9dd447e30ccf8b
gmnochez <gmnochez@outlook.com>
2024-05-10 14:41:35 -0400

    test

--- azurerm/network/security_group/main.tf
+++ azurerm/network/security_group/main.tf
@@ -11,8 +11,10 @@ resource "azurerm_network_security_group" "hadley_resource" {
         direction                  = security_rule.value.direction
         access                     = security_rule.value.access
         protocol                   = security_rule.value.protocol
-        source_port_ranges         = split(",", replace(security_rule.value.source_port_ranges, "*", "0-65535"))
-        destination_port_ranges    = split(",", replace(security_rule.value.destination_port_ranges, "*", "0-65535"))
+        # source_port_ranges         = split(",", replace(security_rule.value.source_port_ranges, "*", "0-65535"))
+        # destination_port_ranges    = split(",", replace(security_rule.value.destination_port_ranges, "*", "0-65535"))
+        source_port_ranges         = security_rule.value.source_port_ranges
+        destination_port_ranges    = security_rule.value.destination_port_ranges        
         source_address_prefix      = security_rule.value.source_address_prefix
         destination_address_prefix = security_rule.value.destination_address_prefix
         description                = security_rule.value.description
