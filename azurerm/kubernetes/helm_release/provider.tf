0cf0590400915dee161e9d203f33f309510a7ed5
gmnochez <gmnochez@outlook.com>
2024-05-14 19:08:31 -0400

    test

--- azurerm/kubernetes/helm_release/provider.tf
+++ azurerm/kubernetes/helm_release/provider.tf
@@ -6,10 +6,19 @@ data "azurerm_kubernetes_cluster" "credentials" {
 
 provider "helm" {
   kubernetes {
-    host                   = data.azurerm_kubernetes_cluster.credentials.kube_config.0.host
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.cluster_ca_certificate)
+    # host                   = data.azurerm_kubernetes_cluster.credentials.kube_config.0.host
+    # client_certificate     = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_certificate)
+    # client_key             = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_key)
+    # cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.cluster_ca_certificate)
+  
+    host                   = data.azurerm_kubernetes_cluster.credentials.endpoint
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["aks", "get-token", "--cluster-name", azurerm_kubernetes_cluster.credentials.name]
+      command     = "az"
+  
+    }
   }
 }
 
