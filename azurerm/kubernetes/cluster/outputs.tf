660dc0e3ad048626039773b65aeff21b4886d87e
gmnochez <gmnochez@outlook.com>
2024-05-14 15:04:29 -0400

    test

--- azurerm/kubernetes/cluster/outputs.tf
+++ azurerm/kubernetes/cluster/outputs.tf
@@ -1,3 +1,8 @@
 output "hadley_resource_azurerm_kubernetes_cluster_id" {
   value = azurerm_kubernetes_cluster.hadley_resource.id
 }
+
+
+output "hadley_resource_azurerm_kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.hadley_resource.name
+}
