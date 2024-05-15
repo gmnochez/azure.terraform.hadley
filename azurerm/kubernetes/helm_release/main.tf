5f1a87af4df2499e05587a2ff8bb5620b85e8ca2
gmnochez <gmnochez@outlook.com>
2024-05-15 11:54:15 -0400

    test

--- azurerm/kubernetes/helm_release/main.tf
+++ azurerm/kubernetes/helm_release/main.tf
@@ -31,11 +31,11 @@ provider "helm" {
     # client_key             = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.client_key)
     # cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config.0.cluster_ca_certificate)
   
-    host                   = data.terraform_remote_state.aks.endpoint
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.aks.certificate_authority.0.data)
+    host                   = data.terraform_remote_state.aks.outputs.endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.aks.outputs.certificate_authority.0.data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["aks", "get-credentials", "--name", data.azurerm_aks_cluster.cluster.name, "-g", data.terraform_remote_state.aks.resource_group_name]
+      args        = ["aks", "get-credentials", "--name", data.azurerm_kubernetes_cluster.cluster.name, "-g", data.terraform_remote_state.aks.outputs.resource_group_name]
       command     = "az"
   
     }
