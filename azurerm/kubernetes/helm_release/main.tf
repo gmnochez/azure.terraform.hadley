db33c7585a3b73d1a294f68c524b196ccdd19beb
gmnochez <gmnochez@outlook.com>
2024-05-15 15:54:57 -0400

    test

--- azurerm/kubernetes/helm_release/main.tf
+++ azurerm/kubernetes/helm_release/main.tf
@@ -55,6 +55,6 @@ resource "helm_release" "hadley_resource" {
 data "kubernetes_service" "hadley_resource" {
   depends_on = [helm_release.hadley_resource]
   metadata {
-    name = "hadley_resource"
+    name = "nginx"
   }
 }
