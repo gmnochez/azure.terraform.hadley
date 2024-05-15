91d2cff7fd7d06d7171cdb023a59c9e3097ec37a
gmnochez <gmnochez@outlook.com>
2024-05-15 16:12:31 -0400

    test

--- azurerm/kubernetes/helm_release/main.tf
+++ azurerm/kubernetes/helm_release/main.tf
@@ -1,19 +1,6 @@
 
 
 
-
-data "terraform_remote_state" "aks" {
-  backend = "azurerm"
-  
-  config = {
-    subscription_id      = var.cluster_subscription_id
-    resource_group_name  = var.cluster_resource_group_name
-    storage_account_name = var.cluster_storage_account_name
-    container_name       = "terraform-state"
-    key                  = var.cluster_storage_key
-  }
-}
-
 data "azurerm_kubernetes_cluster" "cluster" {
   name                = var.name
   resource_group_name = var.resource_group_name
@@ -28,14 +15,6 @@ provider "helm" {
     client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config[0].client_key)
     cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
   
-    # host                   = data.terraform_remote_state.aks.endpoint
-    # cluster_ca_certificate = base64decode(data.terraform_remote_state.aks.certificate_authority.0.data)
-    # exec {
-    #   api_version = "client.authentication.k8s.io/v1beta1"
-    #   args        = ["aks", "get-credentials", "--name", data.azurerm_kubernetes_cluster.cluster.name, "-g", data.terraform_remote_state.aks.resource_group_name]
-    #   command     = "az"
-  
-    # }
   }
 }
 
