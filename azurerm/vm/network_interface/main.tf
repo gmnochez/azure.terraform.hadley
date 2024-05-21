2bea67fcbc602a179d09bf393a5f6ec34ac3729c
gmnochez <gmnochez@outlook.com>
2024-05-20 21:14:41 -0400

    test

--- azurerm/vm/network_interface/main.tf
+++ azurerm/vm/network_interface/main.tf
@@ -11,8 +11,17 @@ resource "azurerm_network_interface_security_group_association" "ktc-association
   network_security_group_id = data.azurerm_network_security_group.nsg.id
 }
 
+
+data "azurerm_network_interface" "netifg" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+}
+
 resource "azurerm_network_interface" "hadley_resource" {
   
+  id                  = data.azurerm_network_interface.netifg.id
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
