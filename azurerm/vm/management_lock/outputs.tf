f10db2fbd11966c85dc0e9269f98e0964ebc122d
gmnochez <gmnochez@outlook.com>
2024-05-21 16:10:18 -0400

    test

--- azurerm/vm/management_lock/outputs.tf
+++ azurerm/vm/management_lock/outputs.tf
@@ -1,9 +1,9 @@
 output "hadley_resource_management_lock_name" {
-  value = azurerm_role_assignment.hadley_resource.name
+  value = azurerm_management_lock.hadley_resource.name
 }
 
 output "hadley_resource_management_lock_id" {
-  value = azurerm_role_assignment.hadley_resource.id
+  value = azurerm_management_lock.hadley_resource.id
 }
 
 
