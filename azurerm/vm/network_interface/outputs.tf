c326eadbfbfd03047d00599a1b6ce8e3a85742d6
gmnochez <gmnochez@outlook.com>
2024-05-21 14:16:31 -0400

    test

--- azurerm/vm/network_interface/outputs.tf
+++ azurerm/vm/network_interface/outputs.tf
@@ -5,8 +5,3 @@ output "hadley_resource_network_interface_name" {
 output "hadley_resource_network_interface_id" {
   value = azurerm_network_interface.hadley_resource.id
 }
-
-
-output "network_interface_id" {
-  value = data.azurerm_network_interface.interface.id
-}
