8ded05d90bb75de6c138056bc3b6ba2d25c6256b
gmnochez <gmnochez@outlook.com>
2024-05-22 16:18:44 -0400

    test

--- azurerm/vm/virtual_machine/outputs.tf
+++ azurerm/vm/virtual_machine/outputs.tf
@@ -1,9 +1,9 @@
-output "hadley_resource_management_lock_name" {
-  value = azurerm_management_lock.hadley_resource.name
+output "hadley_resource_virtual_machine_name" {
+  value = azurerm_virtual_machine.hadley_resource.name
 }
 
-output "hadley_resource_management_lock_id" {
-  value = azurerm_management_lock.hadley_resource.id
+output "hadley_resource_virtual_machine_id" {
+  value = azurerm_virtual_machine.hadley_resource.id
 }
 
 
