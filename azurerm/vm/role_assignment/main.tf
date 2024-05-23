d323762ef4d96fb433d302a88e7c3dda3301990d
gmnochez <gmnochez@outlook.com>
2024-05-23 17:25:56 -0400

    test

--- azurerm/vm/role_assignment/main.tf
+++ azurerm/vm/role_assignment/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_role_assignment" "hadley_resource" {
   for_each = var.role_assignment
   scope                 = each.value.scope
   role_definition_name  = each.value.role_assignment_name
-  principal_id          = each.value.principal_id
+  principal_id          = each.value.role_assignment_id
 }
 
 
