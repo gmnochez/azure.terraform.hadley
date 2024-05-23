b8233dce7582b35c040639f3eb02175d85eb1aaf
gmnochez <gmnochez@outlook.com>
2024-05-23 17:14:39 -0400

    test

--- azurerm/vm/management_lock/outputs.tf
+++ azurerm/vm/management_lock/outputs.tf
@@ -1,9 +1,9 @@
 output "hadley_resource_management_lock_name" {
-  value = azurerm_management_lock.hadley_resource.name
+  value = {for k, v in azurerm_management_lock.hadley_resource: k => v.name}
 }
 
 output "hadley_resource_management_lock_id" {
-  value = azurerm_management_lock.hadley_resource.id
+  value = {for k, v in azurerm_management_lock.hadley_resource: k => v.id}
 }
 
 
