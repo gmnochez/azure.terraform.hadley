a933be1f3ce9c2c44ca1040d8c1ba9fb35d9650a
gmnochez <gmnochez@outlook.com>
2024-05-24 15:30:28 -0400

    test

--- azurerm/network/private_dns/main.tf
+++ azurerm/network/private_dns/main.tf
@@ -1,5 +1,11 @@
+provider "azurerm" {
+  alias           = var.provider_alias
+  subscription_id = var.provider_subscription_id
+  features {}
+}
+
 resource "azurerm_private_dns_a_record" "hadley_resource" {
-  provider            = azurerm.dns
+  provider            = "azurerm.${var.provider_alias}"
   name                = var.name
   zone_name           = var.zone_name
   resource_group_name = var.resource_group_name
