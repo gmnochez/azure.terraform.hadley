7f8f253bafd8eff8a9a8687fae8e3a835b6e2b9d
gmnochez <gmnochez@outlook.com>
2024-05-24 16:02:21 -0400

    test

--- azurerm/network/private_dns/outputs.tf
+++ azurerm/network/private_dns/outputs.tf
@@ -1,7 +1,7 @@
-output "hadley_network_security_group_name" {
-  value = azurerm_network_security_group.hadley_resource.name
+output "hadley_private_dns_a_record_name" {
+  value = {for k, v in azurerm_private_dns_a_record.hadley_resource: k => v.name}
 }
 
-output "hadley_network_security_group_id" {
-  value = azurerm_network_security_group.hadley_resource.id
+output "hadley_private_dns_a_record_id" {
+  value = {for k, v in azurerm_private_dns_a_record.hadley_resource: k => v.id}
 }
