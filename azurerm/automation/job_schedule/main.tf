8765dfc11f4b1c74bcfb3bcf70d98d8adec0a6cb
gmnochez <gmnochez@gmail.com>
2025-09-25 17:19:36 -0300

    fix

--- azurerm/automation/job_schedule/main.tf
+++ azurerm/automation/job_schedule/main.tf
@@ -1,13 +1,14 @@
 
 resource "azurerm_automation_job_schedule" "hadley_resource" {
-  for_each                = var.automation_job_schedules
-  job_name                = each.value.job_name
-  vm_name                 = each.value.vm_name
-  vm_command              = each.value.vm_command
-  rgn_job                 = each.value.rgn_job
-  azure_subscription_id   = each.value.azure_subscription_id
-  action                  = each.value.action
-  
+  for_each = var.automation_job_schedules
+  parameters = {
+    job_name                = each.value.job_name
+    vm_name                 = each.value.vm_name
+    vm_command              = each.value.vm_command
+    rgn_job                 = each.value.rgn_job
+    azure_subscription_id   = each.value.azure_subscription_id
+    action                  = each.value.action
+  }
 
 
   
