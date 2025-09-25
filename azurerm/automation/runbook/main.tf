665d060f6f24f470e10ba1b6197353c1598fb9e2
gmnochez <gmnochez@gmail.com>
2025-09-25 14:50:45 -0300

    test

--- azurerm/automation/runbook/main.tf
+++ azurerm/automation/runbook/main.tf
@@ -1,14 +1,14 @@
 resource "azurerm_automation_runbook" "hadley_resource" {
   for_each = var.automation_runbooks
-  name                    = each.var.name
-  location                = each.var.location
-  resource_group_name     = each.var.resource_group_name
-  automation_account_name = each.var.automation_account_name
-  log_progress            = each.var.log_progress
-  log_verbose             = each.var.log_verbose
-  runbook_type            = each.var.runbook_type
+  name                    = each.value.name
+  location                = each.value.location
+  resource_group_name     = each.value.resource_group_name
+  automation_account_name = each.value.automation_account_name
+  log_progress            = each.value.log_progress
+  log_verbose             = each.value.log_verbose
+  runbook_type            = each.value.runbook_type
   content                 = file("azure_vm_command.ps1")
-  description             = each.var.description
+  description             = each.value.description
 }
 
 
