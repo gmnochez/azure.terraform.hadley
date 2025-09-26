327bea1a6755e07651f7379b13b718462aa0d6d3
gmnochez <gmnochez@gmail.com>
2025-09-26 07:52:08 -0300

    test

--- azurerm/automation/job_schedule/variables.tf
+++ azurerm/automation/job_schedule/variables.tf
@@ -15,7 +15,6 @@ variable "automation_job_schedules" {
       automation_account_name   = string
       resource_group_name       = string
       runbook_name              = string
-      schedule_name             = string
       ResourceGroupName         = string
       AzureSubscriptionID       = string
       Action                    = string
