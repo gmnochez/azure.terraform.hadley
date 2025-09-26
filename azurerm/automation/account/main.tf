a364cdd55a0e753fbdffa02a3b09401cd769ccd5
gmnochez <gmnochez@gmail.com>
2025-09-26 12:50:20 -0300

    test

--- azurerm/automation/account/main.tf
+++ azurerm/automation/account/main.tf
@@ -10,10 +10,20 @@ resource "azurerm_automation_account" "hadley_resource" {
     type = "SystemAssigned"
   }
 
+
   tags = {
     for tag in var.tags:
     tag.key => tag.value
   }
+}
+
+data "azurerm_subscription" "current" {}
+
+resource "azurerm_role_assignment" "hadley_resource" {
+  for_each = azurerm_automation_account.hadley_resource
+  principal_id         = each.value.identity[0].principal_id
+  role_definition_name = "Virtual Machine Contributor"
+  scope                = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${each.value.resource_group_name}"
 }
 
 
